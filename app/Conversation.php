e66b13d5cd2652d8de4d06d9ddcaf94688e0a542
jacquesviljoen <jacques.viljoen@mukuru.com>
2017-12-08 00:05:24 +0200

    Session id

---
+++ app/Conversation.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace MooBot;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Conversation extends Model
+{
+    //
+}
---
+++ database/migrations/2017_12_07_215559_create_conversations_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateConversationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('conversations', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string("sender_id");
+            $table->string("customer_data");
+            $table->string("recipients");
+            $table->string("recipient_id");
+            $table->decimal("amount", 20, 4);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('conversations');
+    }
+}
