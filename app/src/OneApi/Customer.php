209b92d486596b049fa1b156dcfd8ecb7d5e62b8
jacquesviljoen <jacques.viljoen@mukuru.com>
2017-12-07 16:51:06 +0200

    Some api stuff

---
+++ app/src/OneApi/Customer.php
@@ -0,0 +1,32 @@
+<?php namespace OneApi;
+
+class Customer
+{
+
+    protected $data;
+
+    /**
+     * Customer constructor.
+     *
+     * @param array $data
+     */
+    public function __construct(array $data)
+    {
+        $this->data = $data;
+    }
+
+    public function __get($name)
+    {
+        if (!isset($this->data[$name])) {
+           throw new \InvalidArgumentException($name . ' does not exist on the Customer object.');
+        }
+
+        return $this->data[$name];
+    }
+
+    public function __set($name, $value)
+    {
+        $this->data[$name] = $value;
+    }
+
+}
--- config/app.php
+++ config/app.php
@@ -163,7 +163,6 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-
         /*
          * Package Service Providers...
          */
@@ -176,7 +175,7 @@ return [
         // MooBot\Providers\BroadcastServiceProvider::class,
         MooBot\Providers\EventServiceProvider::class,
         MooBot\Providers\RouteServiceProvider::class,
-
+        MooBot\Providers\OneApiClientProvider::class,
     ],
 
     /*
