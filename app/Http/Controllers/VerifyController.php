ff562130e6e5465d85b2542f8509c75a382bfcca
jacquesviljoen <jacques.viljoen@mukuru.com>
2017-12-07 19:38:54 +0200

    remove uneccessary logging

--- app/Http/Controllers/VerifyController.php
+++ app/Http/Controllers/VerifyController.php
@@ -21,8 +21,6 @@ class VerifyController extends Controller
     public function makeItWork(Request $request, ChatbotHelper $chatbotHelper)
     {
         // Facebook webhook verification
-        \Log::info($_REQUEST);
-        \Log::info($request->toArray());
         $chatbotHelper->verifyWebhook($request->toArray());
 
         // Get the fb users data
