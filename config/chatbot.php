5106895638ea687a3d08339fd54948a5eec221a2
jacquesviljoen <jacques.viljoen@mukuru.com>
2017-12-07 19:19:27 +0200

    Change config settings

--- config/chatbot.php
+++ config/chatbot.php
@@ -10,8 +10,8 @@
 */
 
 return [
-    'webhook_verify_token' => env('WEBHOOK_VERIFY_TOKEN'),
-    'access_token'         => env('PAGE_ACCESS_TOKEN'),
+    'webhook_verify_token' => env('MESSENGER_VERIFY_TOKEN'),
+    'access_token'         => env('MESSENGER_APP_TOKEN'),
     'apiai_token'          => env('APIAI_TOKEN'),
     'witai_token'          => env('WITAI_TOKEN'),
 ];
